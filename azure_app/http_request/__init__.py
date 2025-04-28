e80d635e1086eb9372d40d04ca3656c046bb1bf7
pepijnclarijs <pepijnclarijs@hotmail.com>
2025-04-28 16:21:47 +0200

    for testing deployment

--- azure_app/http_request/__init__.py
+++ azure_app/http_request/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import azure.functions as af
 
+# test
 def main(req: af.HttpRequest) -> af.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
     return af.HttpResponse("Hello from Azure Function!", status_code=200)
